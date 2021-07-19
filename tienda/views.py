f40dfb678a2bcca189629227c6e24a2e7e0cc24e
Hans Gramusset Varas <ha.gramusse@duocuc.cl>
2021-07-19 19:20:15 -0400

    correcion 2.0 xd

--- tienda/views.py
+++ tienda/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, ContactoForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Producto
 from .cart import Cart
+from .forms import ContactoForm
+
 
 
 class VistaRegistro(View):
